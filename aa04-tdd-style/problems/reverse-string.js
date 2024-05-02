5404c463c4841adebe11ba4deb27e84ac97450a9
SunTheCoder <sunenglishjr@gmail.com>
2024-05-02 19:14:11 -0400

    solvederror

--- aa04-tdd-style/problems/reverse-string.js
+++ aa04-tdd-style/problems/reverse-string.js
@@ -1,9 +1,17 @@
 function reverseString(string) {
+  if (typeof string !== 'string') {
+    throw TypeError('Argument is not a string.')
+  }
   return string.split('').reverse().join('')
 };
 
 reverseString('fun')
+//reverseString(9)
 
-
+// try {
+//   reverseString
+// } catch (error) {
+//   TypeError('Argument is not a string.')
+// }
 
 module.exports = reverseString
--- aa04-tdd-style/test/reverse-string-spec.js
+++ aa04-tdd-style/test/reverse-string-spec.js
@@ -6,7 +6,7 @@ it('should return a reversed string', function(){
     // this one is storing the reverse string output in a variable then using it
     // const reversed = reverseString('fun')
     // expect(reversed).to.equal('nuf')
-
+    
     // this is checking from the reversed string output in the global scope directly
     expect(reverseString('fun')).to.equal('nuf')
 })
