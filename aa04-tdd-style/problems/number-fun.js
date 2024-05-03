2f35983f096c94ac4e31167f6c5d2d775fad0db2
Ruben Cuello <kuelmusic@gmail.com>
2024-05-02 20:41:39 -0400

    number

--- aa04-tdd-style/problems/number-fun.js
+++ aa04-tdd-style/problems/number-fun.js
@@ -3,11 +3,17 @@ function returnsThree() {
 }
 
 function reciprocal(n) {
+  if(n < 1 || n > 1000000){
+    throw RangeError('Number exceeds range')
+  }
   return 1/n
 }
 
+reciprocal(-1)
+
 //returnsThree()
 
+// since theres more than one export it has to be done in an obj
 module.exports = {
   returnsThree,
   reciprocal
--- aa04-tdd-style/test/number-fun-spec.js
+++ aa04-tdd-style/test/number-fun-spec.js
@@ -1,23 +1,35 @@
 const assert = require('chai')
 const { expect } = require('chai')
 
+// this is the import destructured into an obj because it was exported as an obj
 const { returnsThree } = require('../problems/number-fun')
 const { reciprocal } = require('../problems/number-fun')
 
+// describe is used to give the the the test a header
 describe('returnsThree', function() {
+    // this will appear under the test case above indented like a sub category
     it('Should return 3', function() {
-    
+
         expect(returnsThree()).to.equal(3)
-    }) 
+    })
 })
 describe('reciprocal', function() {
     it('Should return 1 divided by number', function() {
 
         expect(reciprocal(2)).to.equal(1/2)
+        expect(reciprocal(3)).to.equal(1/3)
+        expect(reciprocal(4)).to.equal(1/4)
+    })
+    it('Should return 1 divided by number if number is valid', function() {
+
+        expect(reciprocal(2)).to.equal(1/2)
+        expect(reciprocal(3)).to.equal(1/3)
+        expect(reciprocal(4)).to.equal(1/4)
+    })
+    it('Should return invalid if number exceeds range', function() {
+
+        expect(reciprocal(-1)).to.equal(RangeError())
+        expect(reciprocal(1000001)).to.equal(RangeError())
+
     })
 })
-
-
-
-
-
