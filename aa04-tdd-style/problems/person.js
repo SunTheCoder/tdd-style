fe4853b9f30b759b9114fd40a3a856140f447bbe
SunTheCoder <sunenglishjr@gmail.com>
2024-05-02 20:56:41 -0400

    phase 3 in progress

--- aa04-tdd-style/problems/person.js
+++ aa04-tdd-style/problems/person.js
@@ -1,5 +1,39 @@
 class Person {
-  // Your code here 
+    constructor(name, age) {
+      this.name
+      this.age
+    }
+
+    sayHello(greeting) {
+      return `${this.name} says ${greeting}.`
+    }
+
+    visit(otherPerson) {
+      return `${this.name} visited ${otherPerson}.`
+    }
+
+    switchVisit(otherPerson) {
+      //this.visit()  bind??
+    }
+
+    update(obj) {
+      if (typeof obj !== 'object') {
+        throw TypeError('Argument is not an object!')
+      } else {
+        this.name = obj.name
+        this.age = obj.age
+      }
+    }
+
+    tryUpdate(obj) {
+      
+    }
 }
+
+let coolPerson = new Person("mai", 32); // Person { name: 'mai', age: 32 }
+
+coolPerson.update({ name: "lulu", age: 57 });
+console.log(coolPerson); // Person { name: 'lulu', age: 57 }
+
 
 module.exports = Person;
